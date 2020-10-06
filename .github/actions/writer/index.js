c93fbec44994d0e6176837bd7dc2c232a4b18f1b
Matthew Davis <38021615+mattdavis0351@users.noreply.github.com>
2020-10-06 14:00:52 -0700

    Update index.js

--- .github/actions/writer/index.js
+++ .github/actions/writer/index.js
@@ -6,14 +6,19 @@ async function run() {
     const token = core.getInput("token");
     const octokit = github.getOctokit(token);
     const ctx = github.context;
-    const res = await octokit.repos.createOrUpdateFileContents({
-      owner: ctx.repo.owner,
-      repo: ctx.repo.repo,
-      path: `docs/somefile.md`,
-      message: "initial template setup",
-      content: Buffer.from("some super simple test").toString("base64"),
-      branch: ctx.ref,
-    });
+//     const res = await octokit.repos.createOrUpdateFileContents({
+//       owner: ctx.repo.owner,
+//       repo: ctx.repo.repo,
+//       path: `docs/somefile.md`,
+//       message: "initial template setup",
+//       content: Buffer.from("some super simple test").toString("base64"),
+//       branch: ctx.ref,
+//     });
+    const res = await octokit.repos.getContent({
+  owner: ctx.repo.owner,
+  repo: ctx.repo.repo,
+  branch: ctx.ref,
+});
 
     console.log(res);
   } catch (error) {
