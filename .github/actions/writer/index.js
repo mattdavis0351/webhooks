bcfb79d8a3fd0c70b8f92470439a0e071c3b2408
Matthew Davis <38021615+mattdavis0351@users.noreply.github.com>
2020-10-06 12:29:13 -0700

    Create index.js

---
+++ .github/actions/writer/index.js
@@ -0,0 +1,20 @@
+const github = require("@actions/github");
+const core = require("@actions/core");
+
+async function run() {
+  try {
+    const token = core.getInput("token");
+    const octokit = github.getOctokit(token);
+    const ctx = github.context;
+    const res = await octokit.repos.createOrUpdateFileContents({
+      owner: ctx.repo.owner,
+      repo: ctx.repo.repo,
+      path: path.resolve(`docs/${objectives[i]}.md`),
+      message: "initial template setup",
+      content: Buffer.from("some super simple test").toString("base64"),
+      branch: ctx.ref,
+    });
+  } catch (error) {
+    core.setFailed(error);
+  }
+}
